9080b87c9b8dfb5d7b9a8e7b26ac9e09993a74e2
Ondřej Tůma <ondr@centrum.cz>
2018-08-27 09:52:17 +0200

    get fridays log on monday

--- components/GenerateLogButton/index.jsx
+++ components/GenerateLogButton/index.jsx
@@ -7,8 +7,16 @@ import {getLogs} from '../../helpers/log';
 import styles from './generate-log-button.scss';
 
 class GenerateLogButton extends Component {
+    get isMonday() {
+        return new Date().getDay() === 1;
+    }
+
     generateLog = () => {
-        const logs = getLogs(new Date().setDate(new Date().getDate() - 1));
+        const log_date =  this.isMonday
+            ? new Date().setDate(new Date().getDate() - 3)
+            : new Date().setDate(new Date().getDate() - 1);
+
+        const logs = getLogs(log_date);
 
         if (logs.length === 0) {
             alert('Nothing to show');
@@ -23,7 +31,7 @@ class GenerateLogButton extends Component {
     render() {
         return (
             <div className={styles.wrapper}>
-                <Button label={'Show yesterday\'s log'} onClick={this.generateLog}/>
+                <Button label={`Show ${this.isMonday ? 'friday' : 'yesterday'}'s log`} onClick={this.generateLog}/>
             </div>
         );
     }
